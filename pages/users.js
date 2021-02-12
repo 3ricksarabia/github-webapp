c2ca531f1734a736d295746562f4db946f841318
Erick Sarabia <ecksarabia@gmail.com>
2021-02-12 15:09:53 -0600

    Add github access token from .env

--- pages/users.js
+++ pages/users.js
@@ -1,25 +1,34 @@
 import { useState } from "react";
 import Search from "../components/Search";
+import Navigation from "../components/Navigation";
+import Container from "../containers/User";
 
 const Home = () => {
 	const [users, setUsers] = useState();
+	console.log("process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN", process.env.GITHUB_ACCESS_TOKEN);
 
 	const searchUsers = (event) => {
-		if (event) {
-			fetch(`https://api.github.com/search/users?q=${event.target.value}&per_page=5`, {
+		const USERNAME = event.target.value || "";
+
+		if (USERNAME) {
+			fetch(`https://api.github.com/search/users?q=${USERNAME}&per_page=5`, {
 				headers: {
-					authorization: "token 493080f1411e27c7a0b339e3605e1680d381b331",
+					authorization: `token ${process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN}`,
 				},
 			})
 				.then((response) => response.json())
-				.then((data) => setUsers(data.items));
+				.then((data) => setUsers(data.items))
+				.catch(() => setUsers([]));
+		} else {
+			setUsers([]);
 		}
 	};
 
 	return (
-		<>
+		<Container>
+			<Navigation back="/" />
 			<Search results={users} handleSearch={searchUsers} />
-		</>
+		</Container>
 	);
 };
 
