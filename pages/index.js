a474bcc5add2c50de9500de0d673c92cb780398f
Erick Sarabia <ecksarabia@gmail.com>
2021-02-15 10:50:41 -0600

    Add GitHub logo

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import Link from "next/link";
 import Container from "../components/Container";
+import Logo from "../components/Logo";
 
 const Home = () => (
 	<Container>
+		<Logo alt="GitHub" src="/github.svg" />
 		<h1>GitHub API</h1>
 		<main>
 			<Link href="/users">
