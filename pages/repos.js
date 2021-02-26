d77521d0eb6e2eae0bf8a673999be11c58702aad
Erick Sarabia <ecksarabia@gmail.com>
2021-02-25 23:03:39 -0600

    Add state to storage search results temporarily

--- pages/repos.js
+++ pages/repos.js
@@ -1,23 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Search from "../components/Search";
 import Navigation from "../components/Navigation";
 import Container from "../components/Container";
 
 const Repositories = () => {
-	const [repositories, setRepositories] = useState();
+	const [repositories, setRepositories] = useState([]);
+	const [storage, setStorage] = useState({});
+	const [block, setBlock] = useState(false);
+
+	useEffect(() => {
+		if (Object.keys(storage).length >= 30) {
+			setStorage({});
+		}
+	}, [storage]);
 
 	const searchRepos = (event) => {
 		const REPOSITORY = event.target.value || "";
+		const STORAGE = storage;
 
 		if (REPOSITORY) {
-			fetch(`https://api.github.com/search/repositories?q=${REPOSITORY}&per_page=5`, {
-				headers: {
-					authorization: `token ${process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN}`,
-				},
-			})
-				.then((response) => response.json())
-				.then((data) => setRepositories(data.items))
-				.catch(() => setRepositories([]));
+			if (REPOSITORY in storage) {
+				setRepositories(storage[REPOSITORY]);
+				setBlock(false);
+			} else if (!block) {
+				fetch(`https://api.github.com/search/repositories?q=${REPOSITORY}&per_page=5`, {
+					headers: {
+						authorization: `token ${process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN}`,
+					},
+				})
+					.then((response) => response.json())
+					.then((data) => {
+						if (data.items.length === 0) {
+							setBlock(true);
+						}
+
+						STORAGE[REPOSITORY] = data.items;
+						setRepositories(data.items);
+						setStorage(STORAGE);
+					})
+					.catch(() => setRepositories([]));
+			}
 		} else {
 			setRepositories([]);
 		}
--- pages/users.js
+++ pages/users.js
@@ -1,23 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Search from "../components/Search";
 import Navigation from "../components/Navigation";
 import Container from "../components/Container";
 
 const Users = () => {
-	const [users, setUsers] = useState();
+	const [users, setUsers] = useState([]);
+	const [storage, setStorage] = useState({});
+	const [block, setBlock] = useState(false);
+
+	useEffect(() => {
+		if (Object.keys(storage).length >= 30) {
+			setStorage({});
+		}
+	}, [storage]);
 
 	const searchUsers = (event) => {
 		const USERNAME = event.target.value || "";
+		const STORAGE = storage;
 
 		if (USERNAME) {
-			fetch(`https://api.github.com/search/users?q=${USERNAME}&per_page=5`, {
-				headers: {
-					authorization: `token ${process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN}`,
-				},
-			})
-				.then((response) => response.json())
-				.then((data) => setUsers(data.items))
-				.catch(() => setUsers([]));
+			if (USERNAME in storage) {
+				setUsers(storage[USERNAME]);
+				setBlock(false);
+			} else if (!block) {
+				fetch(`https://api.github.com/search/users?q=${USERNAME}&per_page=5`, {
+					headers: {
+						authorization: `token ${process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN}`,
+					},
+				})
+					.then((response) => response.json())
+					.then((data) => {
+						if (data.items.length === 0) {
+							setBlock(true);
+						}
+
+						STORAGE[USERNAME] = data.items;
+						setUsers(data.items);
+						setStorage(STORAGE);
+					})
+					.catch(() => setUsers([]));
+			}
 		} else {
 			setUsers([]);
 		}
